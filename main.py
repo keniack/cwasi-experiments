eb8d53bbba46661a720b4842c10b054711ad3a62
Cynthia Marcelino <cynthiakenia.arcanjomarcelino@external.drei.com>
2023-05-07 18:04:21 +0200

    first experiments

---
+++ main.py
@@ -0,0 +1,19 @@
+import matplotlib.pyplot as plt
+import pandas as pd
+
+
+def draw():
+
+    filename = "dataset/first-results.csv"
+    colnames = ['Cwasi', 'WasmEdge']
+    df = pd.read_csv(filename, names=colnames,header=None, sep=",",float_precision='high').astype(float)
+    df.head()
+    ax=df.boxplot(grid=True,figsize=(5,7),fontsize=15,whis=[0,100])
+    plt.yticks(fontsize=15)
+    ax.set_ylabel("Seconds")
+    plt.show()
+
+
+
+if __name__ == '__main__':
+    draw()
