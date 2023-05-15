c78ce6548856e6abfbc96623cf592fe02530ace0
Cynthia Marcelino <cynthiakenia.arcanjomarcelino@external.drei.com>
2023-05-15 13:02:20 +0200

    experiments

--- main.py
+++ main.py
@@ -3,11 +3,11 @@ import pandas as pd
 
 
 def draw():
-    filename = "dataset/first-results.csv"
-    colnames = ['Cwasi', 'WasmEdge']
+    filename = "dataset/cwasi-exp400M.csv"
+    colnames = ['C 400M', 'W 400M']
     df = pd.read_csv(filename, names=colnames,header=None, sep=",",float_precision='high').astype(float)
     df.head()
-    ax=df.boxplot(grid=True,figsize=(5,7),fontsize=15,whis=[0,100])
+    ax=df.boxplot(grid=True,figsize=(7,5),fontsize=15,whis=[0,100])
     plt.yticks(fontsize=15)
     ax.set_ylabel("Seconds")
     plt.show()
